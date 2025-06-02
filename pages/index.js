f4bbee594a9c58a3806aa748f2c4a8ae88a0aace
Pedro Cella <49076513+pedro-cella@users.noreply.github.com>
2025-06-02 22:10:03 +0000

    testando git push --force

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Testando o funcionamento do git --amend 💻</h1>
+    return <h1>Testando o funcionamento do git --amend e do push --force 💻</h1>
 }
 
 export default Home;
